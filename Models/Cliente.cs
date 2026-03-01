239b9e2866b593ca13820503cd1edf6aeff6c872
Sofi Crisafulli <71233317+SofiaCrisafulli@users.noreply.github.com>
2026-03-01 11:59:49 -0300

    Cliente con id

--- Models/Cliente.cs
+++ Models/Cliente.cs
@@ -2,6 +2,8 @@ namespace ClientesApi.Models
 {
     public class Cliente
     {
+
+        public int Id { get; set; }
         public string Nombre { get; set; }
         public string Apellido { get; set; }
         public string Direccion { get; set; }
